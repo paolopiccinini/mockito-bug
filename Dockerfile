4c8ef1d638966ef2b1ca7d345d98a1fa3d462ea8
Paolo Piccinini <paolo.piccinini@centricsoftware.com>
2025-03-18 17:54:26 +0100

    removed spring boot maven plugin

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
+FROM alpine/java:21-jdk as build
+COPY mvnw /code/mockito-bug/mvnw
+COPY .mvn /code/mockito-bug/.mvn
+COPY pom.xml /code/mockito-bug/
+
+COPY ./war/pom.xml /code/mockito-bug/war/
+COPY ./war/src /code/mockito-bug/war/
+COPY ./jar/pom.xml /code/mockito-bug/jar/
+COPY ./jar/src /code/mockito-bug/jar/
+
+WORKDIR /code/mockito-bug
+RUN ./mvnw -B org.apache.maven.plugins:maven-dependency-plugin:3.1.2:go-offline
+RUN ./mvnw package
+
+
 ## Stage 2 : create the docker final image
 FROM quay.io/wildfly/wildfly:latest-jdk21
-COPY ./war/target/*.war /opt/jboss/wildfly/standalone/deployments/
-COPY ./jar/target/*.jar /opt/jboss/wildfly/modules/jar/main/
+COPY --from=build /code/mockito-bug/war/target/*.war /opt/jboss/wildfly/standalone/deployments/springbootwildfly.war
+COPY --from=build /code/mockito-bug/jar/target/*.jar /opt/jboss/wildfly/modules/jar/main/
 COPY ./jar/src/main/module.xml /opt/jboss/wildfly/modules/jar/main/
 COPY ./jar/libs/* /opt/jboss/wildfly/modules/jar/main/
 ENV JAVA_TOOL_OPTIONS="-XX:UseSVE=0"
---
+++ war/src/main/java/org/example/HelloController.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import org.junit.platform.commons.util.StringUtils;
+import org.junit.platform.engine.DiscoverySelector;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Paolo")
+public class HelloController {
+
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    String hello() throws ClassNotFoundException {
+        /*Class<?> testClass = Class.forName("org.example.Test");
+        Launcher launcher = LauncherFactory.create();
+        SummaryGeneratingListener summaryGeneratingListener = new SummaryGeneratingListener();
+        XMLRunListener xmlRunListener = new XMLRunListener();
+        LauncherDiscoveryRequest request = getLauncherDiscoveryRequest(testClass, "test");
+        launcher.registerTestExecutionListeners(xmlRunListener, summaryGeneratingListener);
+        launcher.execute(request);
+        summaryGeneratingListener.getSummary().getFailures().forEach(e -> logger.error("s", e.getException()));
+        processTestResults(testClass, summaryGeneratingListener);*/
+        Mockito.mock(Object.class);
+        return "Hi";
+    }
+
+    private void processTestResults(Class<?> testClass, SummaryGeneratingListener listener) {
+        TestExecutionSummary summary = listener.getSummary();
+        long testsFound = summary.getTestsFoundCount();
+        long testsFailed = summary.getTotalFailureCount();
+        long testsSkipped = summary.getTestsSkippedCount();
+
+        if (testsFailed > 0) {
+            logFailedJUnit5Tests(summary);
+            System.out.println(testsFound + " tests: " + testsFailed + " failures, " + testsSkipped + " skipped (" + testClass.getName() + ").");
+        } else {
+            System.out.println(testsFound + " tests completed successfully (" + testClass.getName() + ").");
+        }
+    }
+
+    private void logFailedJUnit5Tests(TestExecutionSummary summary) {
+        summary.getFailures().forEach(failure -> System.out.println(failure.getTestIdentifier().getDisplayName() + " " + failure.getException()));
+    }
+
+    private LauncherDiscoveryRequest getLauncherDiscoveryRequest(Class<?> testClass, String methodName) {
+        DiscoverySelector selector = StringUtils.isBlank(methodName)
+                ? DiscoverySelectors.selectClass(testClass)
+                : DiscoverySelectors.selectMethod(testClass, methodName);
+
+        LauncherDiscoveryRequestBuilder requestBuilder = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selector);
+        requestBuilder = addParallelExecutionParameters(requestBuilder);
+        return requestBuilder.build();
+    }
+
+    private LauncherDiscoveryRequestBuilder addParallelExecutionParameters(LauncherDiscoveryRequestBuilder requestBuilder) {
+        return requestBuilder
+                .configurationParameter(PARALLEL_EXECUTION_ENABLED, "false")
+                .configurationParameter(PARALLELISM_MODE_FOR_MULTIPLE_CLASSES, CONCURRENT);
+    }
+
+    public static final String PARALLEL_EXECUTION_ENABLED            = "junit.jupiter.execution.parallel.enabled";
+
+    /*
+     * Default: same_thread. To enable parallelism, use "concurrent".
+     */
+    public static final String PARALLELISM_MODE_FOR_MULTIPLE_CLASSES = "junit.jupiter.execution.parallel.mode.classes.default";
+
+    public static final String CONCURRENT                            = "concurrent";
+}
